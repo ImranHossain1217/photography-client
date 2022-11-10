ad3ee2479039af08b84c184a148d4ec0a322898e
Imran Hossain <imranhossainemon1717@gmail.com>
2022-11-10 22:19:22 +0600

    delete review

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Review = ({ review }) => {
-  const { name, message, photo } = review;
+  const { _id, name, message, photo } = review;
+
+  const handleReviewDelete = (id) => {
+    const confirm = window.confirm("Are You Sure, Delete Your Review?");
+    if (confirm) {
+      fetch(`http://localhost:5000/reviews/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    }
+  };
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl mt-5">
       <div className="card-body">
@@ -12,7 +24,10 @@ const Review = ({ review }) => {
         </div>
         <h4 className="font-bold">Name: {name}</h4>
         <div className="card-actions justify-end">
-          <button className="btn btn-square btn-sm">
+          <button
+            onClick={() => handleReviewDelete(_id)}
+            className="btn btn-square btn-sm"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
